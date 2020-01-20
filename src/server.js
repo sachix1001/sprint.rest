6a0b45f468e04f11c5a37cbb2d8591d20c1baaf0
travisricks <ricks.travis@gmail.com>
2020-01-20 16:11:37 +0900

    wrote get api/pokemon and post

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 const pokeData = require("./data");
+const express = require("express");
 
 const setupServer = () => {
-  /**
-   * Create, set up and return your express server, split things into separate files if it becomes too long!
-   */
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/api/pokemon", (req, res) => {
+    const n = req.query.limit;
+    if (n !== undefined) {
+      const results = [];
+      // console.log(Array.isArray(pokeData.pokemon));
+      for (let i = 0; i < n; i++) {
+        results.push(pokeData.pokemon[i]);
+      }
+      res.send(results);
+    } else {
+      res.send(pokeData.pokemon);
+    }
+  });
+
+  app.post("/api/pokemon", (req, res) => {
+    pokeData.pokemon.push(req.body);
+    res.send(req.body);
+  });
+
+  return app;
 };
 
 module.exports = { setupServer };
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -2,6 +2,8 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
+chai.should();
+const pokeData = require("../src/data");
 
 /*
  * This sprint you will have to create all tests yourself, TDD style.
@@ -13,5 +15,34 @@ describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
     request = chai.request(server);
+  });
+
+  describe("GET /api/pokemon - returning json", () => {
+    it("should return JSON", async () => {
+      const res = await request.get("/api/pokemon");
+      res.should.be.json;
+    });
+  });
+  describe("GET /api/pokemon - return N numbers of pokemon if limit is defined", () => {
+    it("should return 5 pokemon", async () => {
+      const res = await request.get("/api/pokemon?limit=5");
+      JSON.parse(res.text).length.should.equal(5);
+    });
+  });
+  describe("GET /api/pokemon - return all pokemon if limit is NOT defined", () => {
+    it("should return 151 pokemon", async () => {
+      const res = await request.get("/api/pokemon");
+      JSON.parse(res.text).length.should.equal(151);
+    });
+  });
+
+  describe("POST /api/pokemon - should add a Pokemon", () => {
+    it("should change the data length to 152", async () => {
+      const expected = {
+        id: 152,
+      };
+      const res = await request.post("/api/pokemon").send(expected);
+      pokeData.pokemon.length.should.equal(152);
+    });
   });
 });
